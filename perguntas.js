cb4e21de01e4b5a47c1de52e37cdf918e8556466
thavene moraes <145779967+Moraes2007@users.noreply.github.com>
2024-10-28 12:16:37 -0300

    MORAES

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+  'Programação',
+  'O que é Python?',
+   `O Python e uma linguagem de programação`
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França',
+    'A capital da França é Paris'
+)
+criarCartao( 
+    'progamação',
+    'O que é função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+criarCartao( 
+   'Lingua inglea',
+   'Como se diz oi em inglês?',
+   'Oi em inglẽs é HI(RAI)'
+) 
